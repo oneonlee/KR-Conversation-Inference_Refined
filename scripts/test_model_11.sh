41ad1fafb2bf43da41d295436b1b046a493c2c9a
oneonlee <dg.lee@postech.ac.kr>
2024-08-30 16:07:21 +0000

    update

--- scripts/test_model_11.sh
+++ scripts/test_model_11.sh
@@ -12,7 +12,7 @@ python -m run.test \
     --load_in_4bit \
     --custom_dataset $custom_dataset \
     --src_model_id $model_path \
-    --local_model_path "resource/results/$custom_dataset-$model_prefix-LoRA_2e-4" \
-    --output resource/results/predictions/$custom_dataset-$model_prefix-LoRA_2e-4.json
+    --local_model_path "resource/results/$custom_dataset-$model_prefix-LoRA_1e-4" \
+    --output resource/results/predictions/$custom_dataset-$model_prefix-LoRA_1e-4.json
 
-echo "Model $number Inference Done (Saved at resource/results/predictions/$custom_dataset-$model_prefix-LoRA_2e-4.json)"
+echo "Model $number Inference Done (Saved at resource/results/predictions/$custom_dataset-$model_prefix-LoRA_1e-4.json)"
